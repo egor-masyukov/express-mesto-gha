b462553a2e8d1cac1fad10b39e0a734600510b04
Egor Masyukov <egor-masyukov@yandex.ru>
2023-07-07 14:39:40 +0300

    ПР 13. доработана работа с ошибками

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
+const { httpStatusCode } = require('../errors/httpStatusCode');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
+router.use((req, res) => {
+  res.status(httpStatusCode.NOT_FOUND).send({
+    message: 'Такая страница не существует',
+  });
+});
 
 module.exports = router;
